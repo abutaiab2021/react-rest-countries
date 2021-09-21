5fcfad885e32461610a558b83eed91c578e1dac6
abutaiab2021 <abutaiabcse95@gmail.com>
2021-09-22 01:14:25 +0600

    commited

---
+++ src/components/Country/Country.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Country.css'
+
+const Country = (props) => {
+    // const { name, flag, capital, population} = props.country;
+    const {name, flag, capital, population} = props.country || { } ;
+    // console.log(props.country)
+    return (
+        <div className="country">
+            <h1>Country Name : {name}</h1>
+            <img src={flag} alt="" />
+            <p>Capital : {capital} , Population : {population}</p>
+        </div>
+    );
+};
+
+export default Country;
